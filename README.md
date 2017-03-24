1dd818c9ab37b6e4a368a574c15f54be3ee2dde9
Minh Phuong <mphuong@kent.edu>
2017-03-24 16:53:03 -0700

    add styling

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# question-board
+# Question Board
 
-This README outlines the details of collaborating on this Ember application.
-A short introduction of this app could easily go here.
+An App build by ember to create question and answer
 
 ## Prerequisites
 
--- app/components/answer-update.js
+++ app/components/answer-update.js
@@ -1,14 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  formShow: false,
   actions: {
     updateAns(answer){
       var params = {
         response: this.get('responseUpdate'),
         responder: this.get('responderUpdate'),
       };
-      console.log(params);
+      $("#responderUpdate").val("");
+      $("#responseUpdate").val("");
+      this.set('formShow', false);
       this.sendAction('updateAns', answer, params);
+    },
+    showForm: function(){
+      this.set('formShow', true);
+    },
+    hideForm: function(){
+      this.set('formShow', false);
     },
     deleteAns(answer){
       this.sendAction('deleteAns', answer);
