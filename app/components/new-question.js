29771228f496d3c70be5be78ac545f19111bf414
Minh Phuong <mphuong@kent.edu>
2017-03-24 09:28:11 -0700

    add JS to new question and index

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveQuestion(){
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        note: this.get('note')
+      };
+      this.sendAction('saveQuestion', params)
+    }
+  }
 });
